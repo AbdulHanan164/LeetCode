54d785f66498cd57931e94585c1d82c984fc65ec
Muhammad Abdul Hanan <141907778+AbdulHanan164@users.noreply.github.com>
2024-07-10 17:49:43 +0500

    Count and Say
    The count-and-say sequence is a sequence of digit strings defined by the recursive formula:
    
    countAndSay(1) = "1"
    countAndSay(n) is the run-length encoding of countAndSay(n - 1).
    Run-length encoding (RLE) is a string compression method that works by replacing consecutive identical characters (repeated 2 or more times) with the concatenation of the character and the number marking the count of the characters (length of the run). For example, to compress the string "3322251" we replace "33" with "23", replace "222" with "32", replace "5" with "15" and replace "1" with "11". Thus the compressed string becomes "23321511".
    
    Given a positive integer n, return the nth element of the count-and-say sequence.
    
     
    
    Example 1:
    
    Input: n = 4
    
    Output: "1211"
    
    Explanation:
    
    countAndSay(1) = "1"
    countAndSay(2) = RLE of "1" = "11"
    countAndSay(3) = RLE of "11" = "21"
    countAndSay(4) = RLE of "21" = "1211"
    Example 2:
    
    Input: n = 1
    
    Output: "1"
    
    Explanation:
    
    This is the base case.
    
     
    
    Constraints:
    
    1 <= n <= 30

---
+++ Count and Say.py
@@ -0,0 +1,27 @@
+class Solution(object):
+    def countAndSay(self, n):
+        """
+        :type n: int
+        :rtype: str
+        """
+        if n == 1:
+            return "1"
+        
+        prev_term = "1"
+        
+        for _ in range(2, n + 1):
+            current_term = []
+            i = 0
+            
+            while i < len(prev_term):
+                count = 1
+                while i + 1 < len(prev_term) and prev_term[i] == prev_term[i + 1]:
+                    count += 1
+                    i += 1
+                current_term.append(str(count))
+                current_term.append(prev_term[i])
+                i += 1
+            
+            prev_term = "".join(current_term)
+        
+        return prev_term
